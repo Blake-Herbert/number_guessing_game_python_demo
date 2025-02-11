d444be61be914813a49d443a08426034706bc3c9
Blake <bwherb1@gmail.com>
2025-02-11 18:00:02 -0500

    Initial commit

---
+++ number_guessing_game.py
@@ -0,0 +1,22 @@
+import random
+
+def generate_random():
+    return random.randint(1, 100)
+
+def guess_number(number):
+    guess = None
+    guess = int(input("Guess the number between 1 and 100: \n"))
+    while guess != number:
+        if guess < number:
+            guess = int(input("Too low! Try again.\n"))
+        elif guess > number:
+            guess = int(input("Too high! Try again.\n"))
+    print("Congratulations! You guessed it!")
+    rematch = input("Play again? Y/N\n").lower()
+    if rematch == "y":
+        guess_number(generate_random())
+
+
+
+print("Welcome to the number guessings game!")
+guess_number(generate_random())
