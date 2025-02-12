c372ce117f4fb1a064ffc91973f6fecc691085ce
Blake <bwherb1@gmail.com>
2025-02-12 17:03:42 -0500

    Guess between 1 through 10

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,11 +1,12 @@
 import random
 
 def generate_random():
-    return random.randint(1, 100)
+    return random.randint(1, 10)
 
 def guess_number(number):
     guess = None
-    guess = int(input("Guess the number between 1 and 100: \n"))
+    guess = int(input("Guess the number between 1 and 10: \n"))
+
     while guess != number:
         if guess < number:
             guess = int(input("Too low! Try again.\n"))
@@ -18,5 +19,5 @@ def guess_number(number):
 
 
 
-print("Welcome to the number guessings game!")
+print("Welcome to the number guessing game!")
 guess_number(generate_random())
